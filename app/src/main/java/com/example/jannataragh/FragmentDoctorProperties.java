c821d5afcbf5d44af79d7e5346967b48b82e39bc
hoseinnorouzi <mh.norouzi89@gmail.com>
2019-10-11 18:56:55 +0330

    properties doctor page design done (13)

--- app/src/main/java/com/example/jannataragh/FragmentDoctorProperties.java
+++ app/src/main/java/com/example/jannataragh/FragmentDoctorProperties.java
@@ -7,12 +7,36 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.OvershootInterpolator;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.ms.square.android.expandabletextview.ExpandableTextView;
+
 
 public class FragmentDoctorProperties extends Fragment {
 
+    RelativeLayout relativePropertiesDoctor;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_doctor_properties,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        relativePropertiesDoctor = (RelativeLayout)view.findViewById(R.id.relative_properties_doctor);
+
+        relativePropertiesDoctor.setRotationY(180);
+
+// sample code snippet to set the text content on the ExpandableTextView
+        ExpandableTextView expTv1 = (ExpandableTextView) view.findViewById(R.id.expand_text_view)
+                .findViewById(R.id.expand_text_view);
+
+// IMPORTANT - call setText on the ExpandableTextView to set the text content to display
+        expTv1.setText(getString(R.string.properties_doctor_desc));
+
     }
 }
